4524ea27447935ab260c501725d0e2d46b69c86e
Marcos Antonio Ribeiro <maribeiro9600@gmail.com>
2022-03-21 19:03:32 -0300

    Client entity and seeding, H2 setup

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('João Clementino da Silva', '', 5000.00, TIMESTAMP WITH TIME ZONE '1970-07-14T10:00:00Z', 3 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Gonçalves de Alcantara', '', 9000.00, TIMESTAMP WITH TIME ZONE '1985-11-15T10:00:00Z', 4 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria de Lourdes e Aquino', '', 7000.00, TIMESTAMP WITH TIME ZONE '1982-09-10T10:00:00Z', 2 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Teresa Maria de Jesus', '', 13000.00, TIMESTAMP WITH TIME ZONE '1968-04-13T10:00:00Z', 1 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos Alberto Moraes', '', 4000.00, TIMESTAMP WITH TIME ZONE '1977-08-14T10:00:00Z', 5 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Suely Cristina de Araujo', '', 8000.00, TIMESTAMP WITH TIME ZONE '1980-06-07T10:00:00Z', 2 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Solange Silvestre Silva', '', 11000.00, TIMESTAMP WITH TIME ZONE '1991-04-02T10:00:00Z', 3 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ubaldo dos Santos Nero', '', 8500.00, TIMESTAMP WITH TIME ZONE '1988-03-30T10:00:00Z', 5 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Braulio Mendonça das Neves', '', 12000.00, TIMESTAMP WITH TIME ZONE '1976-05-24T10:00:00Z', 2 );
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Moacir Moreira dos Santos', '', 15000.00, TIMESTAMP WITH TIME ZONE '1993-02-12T10:00:00Z', 2 );
